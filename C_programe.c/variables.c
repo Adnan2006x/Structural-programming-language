a626cd8129715eb6a30acb95272f331227ad5d60
Adnan Sakib <md.adnan2006s@gmail.com>
2026-09-14 00:17:50 +0600

    Add files via upload

---
+++ C_programe.c/variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+
+// Create variables
+int length = 4;
+int width = 6;
+int area;
+
+// Calculate the area
+area = length * width;
+
+// Print the result
+printf("Length is: %d\n", length);
+printf("Width is: %d\n", width);
+printf("Area of the rectangle is: %d", area);
+return 0;
+}
