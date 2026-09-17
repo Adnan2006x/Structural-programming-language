8e576658624d6a5d454b505d50e08ab562ff16d3
Adnan Sakib <md.adnan2006s@gmail.com>
2026-09-18 00:13:52 +0600

    Add files via upload

---
+++ operators/arithmetic/arithmetic2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+
+int a = 10;
+int b = 3;
+printf("%d\n", a / b);   // Integer division, result is 3
+
+double c = 10.0;
+double d = 3.0;
+printf("%f\n", c / d);   // Decimal division, result is 3.333...
+
+return 0;
+}
